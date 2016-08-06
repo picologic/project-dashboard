826b54c800404dc0f83ee20bd6d39cd26194f9ff
Glen Cooper <gcooper@teambrainbox.com>
2016-08-06 13:50:14 -0700

    Added libs to build

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,18 @@ gulp.task('clean', function() {
     return del('./src/public/app');
 });
 
-gulp.task('package', function() {
+gulp.task('package', ['package:app', 'package:libs']);
+
+gulp.task('package:app', function() {
     return gulp.src(['./src/app/**/*', '!./src/app/**/*.ts'])
                .pipe(gulp.dest('./src/public/app'));
+});
+
+gulp.task('package:libs', function() {
+    return gulp.src([
+        'node_modules/core-js/client/shim.min.js',
+        'node_modules/zone.js/dist/zone.js',
+        'node_modules/reflect-metadata/Reflect.js',
+        'node_modules/systemjs/dist/system.src.js'
+    ]).pipe(gulp.dest('./src/public/lib'));
 });
