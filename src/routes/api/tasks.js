d017a8f53b2e64555ab332ad0754793f37010a13
Glen Cooper <gcooper@teambrainbox.com>
2016-08-16 07:26:35 -0700

    Added caching for project id storage

--- src/routes/api/tasks.js
+++ src/routes/api/tasks.js
@@ -2,6 +2,7 @@ var util = require('util');
 var params = require('./param-helper');
 var harvestService = require('../../lib/harvest-service');
 var asanaService = require('../../lib/asana-service');
+var cache = require('../../lib/cache-service');
 
 module.exports.list = function (req, res) {
     var harvest = new harvestService();
@@ -11,17 +12,15 @@ module.exports.list = function (req, res) {
     if (!queryParams.project_id) return res.send(401, 'Missing project id');
 
     // get project name from harvest
-    harvest.Projects.get(queryParams.project_id)
+    getHarvestProject(queryParams.project_id)
         .then(function(p) {
             var name = util.format("[%s] %s", p.code, p.name);
-            return asana.Projects.list({name: name})
-                .then(function(results) {
-                    if (results.length === 0) {
-                        return res.json(404, []);
-                    }
-
-                    var pid = results[0].id;
+            return getAsanaProject(name)
+                .then(function(ap) {
+                    if (!ap) { return res.send(404); }
+                    var pid = ap.id;
                     delete queryParams.project_id;
+
                     return asana.Tasks.list(pid, queryParams)
                         .then(function(response) {
                             return res.json(response);
@@ -29,4 +28,22 @@ module.exports.list = function (req, res) {
                 });
         });
     
+    function getHarvestProject(project_id) {
+        return cache.get('harvest:project:' + project_id,
+            function() { return harvest.Projects.get(project_id); });
+    }
+
+    function getAsanaProject(name) {
+        return cache.get('asana:project:' + name, 
+            function() {
+                return asana.Projects.list({name: name})
+                    .then(function(results) {
+                        if (results.length === 0) {
+                            return null;
+                        }
+
+                        return results[0];
+                    });
+            });
+    }
 };
